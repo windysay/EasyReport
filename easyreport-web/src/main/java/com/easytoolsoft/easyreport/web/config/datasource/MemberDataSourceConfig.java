b81937ad25474a221a2d195488522374c1f7066f
zfc <519225281@qq.com>
2019-12-19 15:38:03 +0800

    切换h2db->mysql&注释数据库初始化代码

--- easyreport-web/src/main/java/com/easytoolsoft/easyreport/web/config/datasource/MemberDataSourceConfig.java
+++ easyreport-web/src/main/java/com/easytoolsoft/easyreport/web/config/datasource/MemberDataSourceConfig.java
@@ -1,16 +1,24 @@
 package com.easytoolsoft.easyreport.web.config.datasource;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
